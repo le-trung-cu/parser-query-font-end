5c8414261f9ad99fee7a203dcf2cdc27a61b7866
Hieu-Trung <trungthgcd17062@fpt.edu.vn>
2022-08-03 11:35:45 +0700

    table

--- src/screen/list-place/ListPlace.js
+++ src/screen/list-place/ListPlace.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
 import style from './ListPlace.module.css';
 
 const listData = (category, name, source) => {
@@ -10,39 +16,116 @@ const listData = (category, name, source) => {
 const rows = [
   listData ("eatery", "quan an", "auto"),
   listData ("eatery", "quan banh beo", "manual (user1)"),
-]
-const ListPlace = () => {
-  const [rowsList, setRowsList] = useState();
+  listData ("d", "3", "xs"),
+];
+
+const descendingComparator = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+const getComparator = (order, orderBy) => {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+const stableSort = (array, comparator) => {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+const headCells = [
+  { id: "category", label: "Place type category"},
+  { id: "name",  label: "Place type name" },
+  { id: "source",  label: "Source" },
+];
+
+const EnhancedTableHead = (props) => {
+  const { order, orderBy, onRequestSort} = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
   return (
-    <Container component="main" fullWidth>
-      <Box>
-        <Typography className={style.titleList} component="h1" variant="h5">List place types</Typography>
-        <Box className={style.boxTable}>
-          <table className={style.tableList}>
-            <tr className={style.tableTitle}>
-              <th>Place type category</th>
-              <th>Place type name</th>
-              <th>Source</th>
-            </tr>
-            <tbody>
-              {
-                (rowsList > 0 ? rows.slice(rowsList) : rows).map((row) => (
-                  <tr key={row.category}>
-                    <td>{row.category}</td>
-                    <td>{row.name}</td>
-                    <td>{row.source}</td>
-                  </tr>
-                ))
-              }
-              <tr >
-                <td colSpan={3} >
-                  <div className={style.buttonExport}><button>Export</button></div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </Box>
-      </Box>
+    <TableHead className ={style.tableTitle}>
+      <TableRow className ={style.tableHeader}>
+        {headCells.map(headCell => (
+          <TableCell className={style.tableCell}
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "default"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={style.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+const ListPlace = () => {
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("name");
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+  return (
+    <Container fullWidth>
+      <Typography className={style.titleList} component="h1" variant="h5"> List place type</Typography>
+         <TableContainer className={style.tableList} >
+          <Table>
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody >
+              {stableSort(rows, getComparator(order, orderBy)).map(
+                (row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  return (
+                    <TableRow className={style.tablerow}>
+                      <TableCell className={style.tablerow} align="left" id={labelId}>{row.category}</TableCell>
+                      <TableCell className={style.tablerow} align="left">{row.name}</TableCell>
+                      <TableCell className={style.tablerow} align="left">{row.source}</TableCell>
+                    </TableRow>
+                  );
+                }
+              )}
+              <TableRow>
+                <TableCell colSpan={3} className={style.tablerow}>
+                  <div className={style.buttonExport}>
+                    <button>Export</button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
     </Container>
   )
 }
--- src/screen/review-place/ReviewPlace.js
+++ src/screen/review-place/ReviewPlace.js
@@ -1,51 +1,106 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { DataGrid } from '@mui/x-data-grid';
 import style from './ReviewPlace.module.css'
-import { Box } from '@mui/system';
 
-const createData = (category, name, source, approve, discard) => {
-  return { category, name, source, approve, discard };
-}
 
-const rows = [
-  createData("eatery", "quan an", "auto", "Approve", "Discard"),
-  createData("earery", "quan banh beo", "user1", "Approve", "Discard"),
-]
+const useSortableData = (items, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const sortedItems = React.useMemo(() => {
+    let sortableItems = [...items];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [items, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { items: sortedItems, requestSort, sortConfig };
+};
+
+const ProductTable = (props) => {
+  const { items, requestSort, sortConfig } = useSortableData(props.products);
+  const getClassNamesFor = (category) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === category ? sortConfig.direction : undefined;
+  };
+  return (
+    <TableContainer className={style.tableReview}>
+      <Table>
+      <TableHead className={style.tableTitle}>
+        <TableRow className={style.tablerow}>
+          <TableCell className={style.tableCell} onClick={() => requestSort("category")}>Place type category</TableCell>
+          <TableCell className={style.tableCell} onClick={() => requestSort("name")}>Place type name</TableCell>
+          <TableCell className={style.tableCell} onClick={() => requestSort("source")}>Source</TableCell>
+          <TableCell className={style.tableCell} colSpan={2}>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {items.map((item) => (
+          <TableRow key={item.id} className={style.tablerow}>
+            <TableCell className={style.tablerow}>{item.category}</TableCell>
+            <TableCell className={style.tablerow}>{item.name}</TableCell>
+            <TableCell className={style.tablerow}>{item.source}</TableCell>
+            <TableCell className={style.tablerow}>{item.approve}</TableCell>
+            <TableCell className={style.tablerow}>{item.discard}</TableCell>
+          </TableRow>
+        ))}
+        <TableRow  className={style.tablerow}>
+          <TableCell colSpan={5} className={style.tablerow}></TableCell>
+        </TableRow>
+      </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 const ReviewPlace = () => {
-  const [rowsPerPage, setRowsPerPage] = useState()
   return (
-    <Container component="main" fullWidth>
-      <Typography className={style.titleReview} component="h1" variant="h5"> Review place types</Typography>
-      <Box className={style.boxReview}>
-        <table className={style.tableReview} >
-          <tr className={style.headerTabel}>
-            <th>Place type category</th>
-            <th>Place type name</th>
-            <th>Source</th>
-            <th colSpan={2}>Action</th>
-          </tr>
-          <tbody>
-            {(rowsPerPage > 0 ? rows.slice(rowsPerPage) : rows).map((row) => (
-              <tr key={row.category}>
-                <td>{row.category}</td>
-                <td>{row.name}</td>
-                <td>{row.source}</td>
-                <td>{row.approve}</td>
-                <td>{row.discard}</td>
-              </tr>
-            ))}
-            <tr className={style.trEnd}>
-              <td colSpan={5} ></td>
-            </tr>
-          </tbody>
-        </table>
-      </Box>
-
+    <Container fullWidth>
+    <Typography component="h1" variant="h5" className={style.titleReview}>Review place types</Typography>
+      <ProductTable
+        products={[
+          { id: 1, category: "eatery", name: "quan an", source: "Auto", approve: "Approve", discard: "Discard" },
+          { id: 2, category: "eatery", name: "quan banh beo", source: "manual (user1)", approve: "Approve", discard: "Discard" },
+          { id: 3, category: "Yoghurt", name: "atm", source: "hi", approve: "Approve", discard: "Discard" },
+          { id: 4, category: "eatery", name: "quan an", source: "Auto", approve: "Approve", discard: "Discard" },
+          { id: 5, category: "eatery", name: "quan banh beo", source: "manual (user1)", approve: "Approve", discard: "Discard" },
+          { id: 6, category: "Yoghurt", name: "atm", source: "hi", approve: "Approve", discard: "Discard" },
+        ]}
+      />
     </Container>
-  )
+  );
 }
-
 export default ReviewPlace
 
+
+/* https://codesandbox.io/s/table-sort-lmmib?file=/src/styles.css */
